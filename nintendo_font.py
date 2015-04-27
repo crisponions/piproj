9f4d7312ba6b096f056e70d1ccf9a9acaa3fe330
Dave Phillips <dave@vm-14.04.crisponions.com>
2015-04-26 20:06:56 -0700

    changed to using dictionary for storing alphabet

--- nintendo_font.py
+++ nintendo_font.py
@@ -30,160 +30,38 @@ class NintendoFont:
 
     def __init__(self, matrix):
         self.matrix = matrix
-        
-    def a(self):
-        self.matrix.writeRowRaw(0,0x001C)
-        self.matrix.writeRowRaw(1,0x0036)
-        self.matrix.writeRowRaw(2,0x0063)
-        self.matrix.writeRowRaw(3,0x0063)
-        self.matrix.writeRowRaw(4,0x007F)
-        self.matrix.writeRowRaw(5,0x0063)
-        self.matrix.writeRowRaw(6,0x0063)
-    def b(self):
-        self.matrix.writeRowRaw(0,0x003f)
-        self.matrix.writeRowRaw(1,0x0063)
-        self.matrix.writeRowRaw(2,0x0063)
-        self.matrix.writeRowRaw(3,0x003f)
-        self.matrix.writeRowRaw(4,0x0063)
-        self.matrix.writeRowRaw(5,0x0063)
-        self.matrix.writeRowRaw(6,0x003f)
-    def c(self):
-        self.matrix.writeRowRaw(0,0x003c)
-        self.matrix.writeRowRaw(1,0x0066)
-        self.matrix.writeRowRaw(2,0x0003)
-        self.matrix.writeRowRaw(3,0x0003)
-        self.matrix.writeRowRaw(4,0x0003)
-        self.matrix.writeRowRaw(5,0x0066)
-        self.matrix.writeRowRaw(6,0x003c)
-    def d(self):
-        self.matrix.writeRowRaw(0,0x003f)
-	self.matrix.writeRowRaw(1,0x0033)
-        self.matrix.writeRowRaw(2,0x0063)
-        self.matrix.writeRowRaw(3,0x0063)
-        self.matrix.writeRowRaw(4,0x0063)
-        self.matrix.writeRowRaw(5,0x0033)
-        self.matrix.writeRowRaw(6,0x003f)
-
-    def e(self):
-        self.matrix.writeRowRaw(0,0x007f)
-        self.matrix.writeRowRaw(1,0x0003)
-        self.matrix.writeRowRaw(2,0x0003)
-        self.matrix.writeRowRaw(3,0x003f)
-        self.matrix.writeRowRaw(4,0x0003)
-        self.matrix.writeRowRaw(5,0x0003)
-        self.matrix.writeRowRaw(6,0x007f)
-
-    def f(self):
-        pass
-    def g(self):
-        self.matrix.writeRowRaw(0,0x001f)
-        self.matrix.writeRowRaw(1,0x0030)
-        self.matrix.writeRowRaw(2,0x0060)
-        self.matrix.writeRowRaw(3,0x0067)
-        self.matrix.writeRowRaw(4,0x0063)
-        self.matrix.writeRowRaw(5,0x0033)
-        self.matrix.writeRowRaw(6,0x001f)
-    def h(self):
-        pass
-    def i(self):
-        pass
-    def j(self):
-        pass
-    def k(self):
-        pass
-    def l(self):
-        self.matrix.writeRowRaw(0,0x0003)
-        self.matrix.writeRowRaw(1,0x0003)
-        self.matrix.writeRowRaw(2,0x0003)
-        self.matrix.writeRowRaw(3,0x0003)
-        self.matrix.writeRowRaw(4,0x0003)
-        self.matrix.writeRowRaw(5,0x0003)
-        self.matrix.writeRowRaw(6,0x007f)
-
-    def m(self):
-        pass
-    def n(self):
-        pass
-    def o(self):
-        self.matrix.writeRowRaw(0,0x003e)
-        for num in range(1,6):
-             self.matrix.writeRowRaw(num,0x0063)
-        self.matrix.writeRowRaw(6,0x003e)
-    def p(self):
-        pass
-    def q(self):
-        pass
-    def r(self):
-        self.matrix.writeRowRaw(0,0x003f)
-        self.matrix.writeRowRaw(1,0x0063)
-        self.matrix.writeRowRaw(2,0x0063)
-        self.matrix.writeRowRaw(3,0x0073)
-        self.matrix.writeRowRaw(4,0x001f)
-        self.matrix.writeRowRaw(5,0x003b)
-        self.matrix.writeRowRaw(6,0x0073)
-    def s(self):
-        return ('0x003e', '0x0063', '0x0003', '0x003e', '0x0060', '0x0063', '0x003e')
-        #self.matrix.writeRowRaw(0,0x003e)
-        #self.matrix.writeRowRaw(1,0x0063)
-        #self.matrix.writeRowRaw(2,0x0003)
-        #self.matrix.writeRowRaw(3,0x003e)
-        #self.matrix.writeRowRaw(4,0x0060)
-        #self.matrix.writeRowRaw(5,0x0063)
-        #self.matrix.writeRowRaw(6,0x003e)
-    def t(self):
-        pass
-    def u(self):
-        pass
-    def v(self):
-        pass
-    def w(self):
-        pass
-    def x(self):
-        pass
-    def y(self):
-        pass
-    def z(self):
-        pass
-    
-    def print_letter(self, letter):
-        function_dict = {
-                         'a': self.a,
-                         'b': self.b,
-                         'c': self.c,
-                         'd': self.d,
-                         'e': self.e,
-                         'f': self.f,
-                         'g': self.g,
-                         'h': self.h,
-                         'i': self.i,
-                         'j': self.j,
-                         'k': self.k,
-                         'l': self.l,
-                         'm': self.m,
-                         'n': self.n,
-                         'o': self.o,
-                         'p': self.p,
-                         'q': self.q,
-                         'r': self.r,
-                         's': self.s,
-                         't': self.t,
-                         'u': self.u,
-                         'v': self.v,
-                         'w': self.w,
-                         'x': self.x,
-                         'y': self.y,
-                         'z': self.z,
+         
+        self.letter_dict = {
+                         'a': ['1c', '36', '63', '63', '7f', '63', '63'],
+                         'b': ['3f', '63', '63', '3f', '63', '63', '3f'],
+                         'c': ['3c', '66', '03', '03', '03', '66', '3c'],
+                         'd': ['3f', '33', '63', '63', '63', '33', '3f'],
+                         'e': ['7f', '03', '03', '3f', '03', '03', '7f'],
+                         'f': [],
+                         'g': ['1f', '30', '60', '67', '63', '33', '1f'],
+                         'h': [],
+                         'i': [],
+                         'j': [],
+                         'k': [],
+                         'l': ['03', '03', '03', '03', '03', '03', '7f'],
+                         'm': [],
+                         'n': [],
+                         'o': ['3e', '63', '63', '63', '63', '63', '3e'],
+                         'p': [],
+                         'q': [],
+                         'r': ['3f', '63', '63', '73', '1f', '3b', '73'],
+                         's': ['3e', '63', '03', '3e', '60', '63', '3e'],
+                         't': [],
+                         'u': [],
+                         'v': [],
+                         'w': [],
+                         'x': [],
+                         'y': [],
+                         'z': [],
                        }
-        try:
-            function = function_dict[letter]
-            function()
-            #eval(function)
-        except KeyError:
-            print 'Invalid Function'
-
-
-    def test_letter(self, letter_array):
+    def test_letter(self, letter):
         
+        letter_array = self.letter_dict[letter]
         def draw(bin_num):
             row = ''
             for num in bin_num:
