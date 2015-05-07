edac9d655ab9110aaeb6c26e4edd739ac268e300
Dave Phillips <crisponions@msn.com>
2015-05-06 20:10:20 -0700

    almost done

--- draw_test.py
+++ draw_test.py
@@ -3,10 +3,11 @@ import sys
 from nintendo_font import NintendoFont as nf
 
 s = nf('hello')
-def print_letter(f):
-    for i in f:
-        s.test_letter(i)
+#def print_letter(f):
+#    for i in f:
+#        s.test_letter(i)
 
 if __name__ == '__main__':
-    print_letter(sys.argv[1])
+    for j in sys.argv[1]:
+        s.print_letter(j)
 
